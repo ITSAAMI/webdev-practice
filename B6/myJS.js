71c178b1fb2882dfb9cf87a6629a7732dcf8e3d5
ITSAAMI <itsaami42@gmail.com>
2025-08-12 16:57:30 +0500

    All Class

---
+++ B6/myJS.js
@@ -0,0 +1,23 @@
+function showDiv(){
+
+
+    var div1 = document.getElementById('div');
+            // div1.innerHTML = "I am JS";
+            // div1.style.color = "red";
+            // div1.style.display = " none";
+            // div1.style.display = "block";
+            // console.log("asdb fasd fkasd jf");
+
+
+
+            // document.write("I am JS");
+            // console.dir(window);
+
+
+            // alert("i am Alert");
+            // prompt("what is your name?");
+            
+        }
+
+
+
